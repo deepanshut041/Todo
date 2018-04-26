470944175f11903b41b11de627df1dd4b2dff5d8
deepanshut041 <deepanshut041@gmail.com>
2018-04-26 16:25:53 +0530

    changed login activity code now finshing

--- app/src/main/java/com/silive/deepanshu/todoapp/LoginActivity.java
+++ app/src/main/java/com/silive/deepanshu/todoapp/LoginActivity.java
@@ -1,13 +1,151 @@
 package com.silive.deepanshu.todoapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.silive.deepanshu.todoapp.models.UserModel;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 1;
+    public Fragment currentFragment;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView navigationView;
+    private View navHeader;
+    private TextView navHeaderName, navHeaderEmail;
+    private CharSequence mTitle, mDrawerTitle;
+    private ImageView navHeaderImg;
+    private int navItemIndex;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                if (firebaseUser != null) {
+                    String username = firebaseUser.getDisplayName();
+                    String uid = firebaseUser.getUid();
+                    String email = firebaseUser.getEmail();
+                    String photo_url = "http://www.africanleadershipawards.com/wp-content/uploads/2017/07/no-avatar-user.jpg";
+                    Uri photo = firebaseUser.getPhotoUrl();
+                    if(photo != null){
+                        photo_url = firebaseUser.getPhotoUrl().toString();
+                    }
+                } else {
+                    //onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .setTheme(R.style.YellowTheme)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "User Signed in!!", Toast.LENGTH_SHORT).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Sign in canceled!!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    public void onSignedInInitialize(String user_name, String user_id, String user_email, String user_image) {
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("user_id", user_id);
+        editor.putString("user_email", user_email);
+        editor.putString("user_name", user_name);
+        editor.putString("user_image",user_image );
+        Log.e("user image",user_image);
+        editor.apply();
+    }
+
+    public void addUser(final String uid, final String user_name,final String email,final String imgUrl){
+//        final FirebaseCurd firebaseCurd = new FirebaseCurd(MainActivity.this);
+//        DatabaseReference mUserReference = firebaseCurd.getmUsersReference().child(uid);
+//        mUserReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                UserModel userModel = dataSnapshot.getValue(UserModel.class);
+//                if (userModel == null){
+//                    UserModel addUserModel = new UserModel(uid,user_name,email,imgUrl);
+//                    firebaseCurd.addUserModel(addUserModel);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
+
+    }
+
 }
